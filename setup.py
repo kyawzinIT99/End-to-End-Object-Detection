bcc52955893ce277aa68ec4d4877bbbd43447830
Berry <berry@Berrys-MacBook-Pro.local>
2025-08-23 18:46:31 +0700

    updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'signLanguages',
     version= '0.0.0',
-    author= 'iNeuron',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'kyawzinIT99',
+    author_email= 'kyawzin.ccna@gmail.com',
     packages= find_packages(),
     install_requires = []
 
